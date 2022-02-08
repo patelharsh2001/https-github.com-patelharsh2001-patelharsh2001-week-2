33031158f5201de461742c90827a992a9e8e02a4
patelharsh2001 <pharsh1390@gmail.com>
2022-02-08 12:31:04 -0500

    updated

--- routes/index.js
+++ routes/index.js
@@ -8,21 +8,26 @@ router.get('/', function(req, res, next) {
 
 /* GET home page. */
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('../views/partials/webpages/home.ejs', { title: 'Home' });
 });
 
 /* GET About page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+  res.render('../views/partials/webpages/about.ejs', { title: 'About' });
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('../views/partials/webpages/services.ejs', { title: 'Services' });
+});
+
+/* GET Projects page. */
+router.get('/project', function(req, res, next) {
+  res.render('../views/partials/webpages/projects.ejs', { title: 'Projects' });
 });
 
 /* GET Contacts page. */
 router.get('/contacts', function(req, res, next) {
-  res.render('index', { title: 'Contacts' });
+  res.render('../views/partials/webpages/contacts.ejs', { title: 'Contacts' });
 });
 module.exports = router;
